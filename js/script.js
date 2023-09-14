46562f1cd04cd342e3f908bc7f94f01d0d3c74be
= <Heitor.Macedo2@rhimagnesita.com>
2023-09-14 16:32:16 -0300

    update toLowerCase

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const form = document.querySelector('.form')
 const input = document.querySelector('.input_search')
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
     const data = await APIResponse.json();
 
     console.log(data)
@@ -22,6 +22,6 @@ const renderPokemon = async (pokemon) => {
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    renderPokemon(input.value.toLowerCase())
+    renderPokemon(input.value)
     input.value = ''
 })

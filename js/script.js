2a1e5f179cbd2b9d4c8ff6e0281b7556cf7cdfe8
= <Heitor.Macedo2@rhimagnesita.com>
2023-09-14 16:43:34 -0300

    added support to not found pokemon

--- js/script.js
+++ js/script.js
@@ -7,17 +7,27 @@ const input = document.querySelector('.input_search')
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
-    const data = await APIResponse.json();
+    // console.log(APIResponse.status)
 
-    console.log(data)
-    return data;
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...'
     const data = await fetchPokemon(pokemon)
-    pokemonName.innerHTML = data.name
-    pokemonNumber.innerHTML = data.id
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+    if (data) {
+        pokemonName.innerHTML = data.name
+        pokemonNumber.innerHTML = data.id
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    } else {
+        pokemonName.innerHTML = '[Not found]'
+        pokemonNumber.innerHTML = ''
+        pokemonImage.src = []
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -25,3 +35,5 @@ form.addEventListener('submit', (event) => {
     renderPokemon(input.value)
     input.value = ''
 })
+
+renderPokemon('1')

b0cd106d74ba7dbb6ca3c43a9b82e6923ae1efac
= <Heitor.Macedo2@rhimagnesita.com>
2023-09-14 16:13:57 -0300

    added number and name from pokemon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const pokemonName = document.querySelector('.pokemon_name')
+const pokemonNumber = document.querySelector('.pokemon_number')
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     const data = await APIResponse.json();
+
+    console.log(data)
 
     return data;
 }
@@ -10,6 +13,7 @@ const fetchPokemon = async (pokemon) => {
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon)
     pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML += data.id
 }
 
-renderPokemon('25')
+renderPokemon('12')
